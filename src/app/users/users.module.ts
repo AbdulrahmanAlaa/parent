79d8e2484c5f90eee1eeac7ac8e12edc62e89402
DESKTOP-7CI91VE\Abdulrahman Alaa <abdelraman.bashir@vodafone.com>
2017-11-16 09:25:14 +0200

    Creating User and Auth Skelton

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { UsersRoutingModule } from './users-routing.module';
+import { AddComponent } from './add/add.component';
+import { UsersComponent } from './users.component';
+import { ListComponent } from './list/list.component';
+import { SingleComponent } from './single/single.component';
+import { EditComponent } from './edit/edit.component';
+import { DeleteComponent } from './delete/delete.component';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    UsersRoutingModule
   ],
-  declarations: []
+  declarations: [AddComponent, UsersComponent, ListComponent, SingleComponent, EditComponent, DeleteComponent]
 })
 export class UsersModule { }
